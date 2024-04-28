0eac6a82dad7e17a38a8abb0053b0460b462925b
Serhan Chavdarliev <67830778+darimachine@users.noreply.github.com>
2024-04-28 21:08:14 +0300

    Update main.cpp

--- HomeWork2/Task2/main.cpp
+++ HomeWork2/Task2/main.cpp
@@ -1,52 +1,75 @@
-// Domashno_2_ZAD_2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Domashno_2_ZAD2_Beter.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
 #include "ModifiableIntegersFunction.h"
+
+
 using std::cout;
 using std::endl;
 
-int main()
+int main() // everything works perfect!!!!!!
 {
-    
     try {
        
-        ModifiableIntegersFunction a([](int16_t x)->int16_t {return 2*x+3; });
         
-        a.addSpecialNumber(3, 10);
-        //a.excludePoint(4);
-        //a.excludePoint(4);
-        ModifiableIntegersFunction b([](int16_t x)->int16_t {return 2*x + 5; });
-        cout << a.callFunction(3);
-        cout << endl;
-        cout << a.inverse(13);
-        cout << endl;
-        cout << (a || b);
-        cout << endl;
-        ModifiableIntegersFunction c = a - b;
-        cout << c.callFunction(13);
-        cout << endl;
-        cout<<(a ^ 3)(9); // operator ^
-        cout << endl;
-      
-        //ModifiableIntegersFunction k = a(b);
-        cout << endl;
-        //cout << k.callFunction(4);
-        cout << endl;
-        cout << c.callFunction(4);
-        cout << endl;
-        cout << INT16_MAX;
-    }
-    catch (const std::exception& er)
-    {
-        cout<<er.what();
-    }
-    catch (...)
-    {
-        cout << "Unexpected Error";
-    }
+        ModifiableIntegersFunction a([](int16_t x)->int16_t {return x + 3; });
+        ModifiableIntegersFunction b([](int16_t x)->int16_t {return x + 5; });
 
-  
-    //std::cout << "Hello World!\n";
+        ModifiableIntegersFunction c = a + b; // sum of two
+        ModifiableIntegersFunction d = c + b; // chain sum
+        cout << a(2) << endl;// test a
+        cout << b(2) << endl;//test  b
+        cout << c(2) << endl;//test c
+        cout << d(2) << endl;// test d
+        ModifiableIntegersFunction h = d ^ 3; // test ^       
+        cout << h(2) << endl; // test
+        ModifiableIntegersFunction f([](int16_t x)->int16_t {return x + 6; });
+        plotFunction(f, -10, 10, -10, 10); // plot function
+        cout << f(3) << endl;
+        f.serialize("serhan.dat"); // saving RANGE OF f()
+        ModifiableIntegersFunction myObj([](int16_t x)->int16_t {return x + 10; });
+        cout << myObj(6) << endl; //printing regular RANGE
+        myObj.deserialize("serhan.dat"); // Reading RANGE of f()
+        cout << myObj(6) << endl; // printing Range From Readed FILE working!!!
+
+        ModifiableIntegersFunction fInverse = inverse(f); // get inverse comment bijective check if you want to work on any function
+
+
+        cout << f.getInjective() << endl; // check if is injective
+        cout << f.getSurjective() << endl; // check if is surjective
+        f.addSpecificNumber(3, 20); // you can add specificNumber
+        cout << f.getInjective() << endl; // changed!!!
+        f.excludePoint(5); // you can exlclude number
+        cout << f(3) << endl;
+        cout << fInverse(7); // print inverse
+         cout << endl;
+        cout << f(6) << endl;
+
+        ModifiableIntegersFunction g([](int16_t x)->int16_t {return 2 * x + 5; });
+         cout << (f || g) << endl; // check if they are paraller
+         cout << (f < g) << endl; // all comparison operators
+         cout << f(g(4)) << endl; //compose of MULTIPLE function
+
+        ModifiableIntegersFunction k = f + g; // create their sum
+        cout << k(2);
+        cout << endl;
+        ModifiableIntegersFunction compose = k ^ 3; // operator ^ will be called 3 times
+        cout << compose(2);
+        cout << endl;
+        ModifiableIntegersFunction o = k + g; // sum of sums
+        cout << o(2);
+        
+            
+    }
+    catch (std::runtime_error& er)
+    {
+        cout << er.what();
+    }
+    catch (std::exception& er)
+    {
+        cout << er.what();
+    }
+    
 }
 

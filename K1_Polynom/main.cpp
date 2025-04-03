cb48259c578ef96b574dd4a26bc928c5a6140ed4
Serhan Chavdarliev <serhi1334@gmail.com>
2025-04-03 23:30:47 +0300

    Update main.cpp

--- K1_Polynom/main.cpp
+++ K1_Polynom/main.cpp
@@ -5,16 +5,16 @@ int main()
 {
 		Polynom p3(5);
 		p3.print();
-		//double arr[3] = { 2,6,1 }; // 2 + 6x^1 + 1x^2
-		//Polynom p1(nullptr, 3);
-		//p1.print();
-		//double arr2[2] = { 2,6 }; // 2 + 6x^1 + 1x^2
-		//Polynom p2(arr2, 2);
-		//p2.load("file.txt");
-		//p2.print();
-		//cout << endl;
-		//p1.add(p2);
-		//p1.print();
+		double arr[3] = { 2,6,1 }; // 2 + 6x^1 + 1x^2
+		Polynom p1(nullptr, 3);
+		p1.print();
+		double arr2[2] = { 2,6 }; // 2 + 6x^1 + 1x^2
+		Polynom p2(arr2, 2);
+		p2.load("file.txt");
+		p2.print();
+		cout << endl;
+		p1.add(p2);
+		p1.print();
 	
 	
 }
